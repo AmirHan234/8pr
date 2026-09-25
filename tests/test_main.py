4678a2953c67fcd5b9e6cf7b13d9aec2a4a629f5
AmirHan234 <myamir0714@gmail.com>
2026-09-25 10:10:20 +0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 
 def test_calculate_bmi_success():
-    # Проверяем расчет для человека весом 70 кг и ростом 1.75 м
+    # Проверяем расчет для человека весом 70 кг и ростом 1.75 
     response = client.get("/bmi?weight=70&height=1.75")
     assert response.status_code == 200
     assert response.json()["bmi"] == 22.86
